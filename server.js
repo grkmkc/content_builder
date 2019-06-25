303478e6fed24626e735d17ed851d998acf03fd4
gorkem koc <gorkem.koc48@gmail.com>
2019-06-25 22:56:03 +0300

    first routes

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = express();
 
 //Connect mongo
 connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
